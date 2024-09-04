7abec2e2f98d1a3a0b4b1290f323408a35b6713e
stormphoenix <stormphoenix.hzau@hotmail.com>
2024-09-04 11:19:45 +0800

    feature(Project): 更新构建脚本到 UE5.4 版本。
    (cherry picked from commit 071b337f7f71ff703c25d3993f92b40f44198fec)

--- OriginalGame.Target.cs
+++ OriginalGame.Target.cs
@@ -5,11 +5,12 @@ using System.Collections.Generic;
 
 public class OriginalGameTarget : TargetRules
 {
-	public OriginalGameTarget( TargetInfo Target) : base(Target)
+	public OriginalGameTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		CppStandard = CppStandardVersion.Default;
 		ExtraModuleNames.Add("OriginalGame");
 	}
 }
--- OriginalGameEditor.Target.cs
+++ OriginalGameEditor.Target.cs
@@ -5,11 +5,12 @@ using System.Collections.Generic;
 
 public class OriginalGameEditorTarget : TargetRules
 {
-	public OriginalGameEditorTarget( TargetInfo Target) : base(Target)
+	public OriginalGameEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
-		ExtraModuleNames.Add("OriginalGame");
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		CppStandard = CppStandardVersion.Default;
+		ExtraModuleNames.AddRange( new string[] { "OriginalGame" } );
 	}
 }
